6d869e492919a8d6a1ca19cb2a6bdbdc5daec4d4
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-09-26 15:15:02 +1000

    Change module docstring

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,6 +1,6 @@
 """
 CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
+Temperature conversion program
 """
 MENU = """C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"""
 
