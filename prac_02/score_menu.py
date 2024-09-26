73bdd77a193aec61ff83342739aab6c6a9b923fe
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-09-26 15:20:39 +1000

    Add main function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,3 +2,19 @@
 CP1404/CP5632 - Practical
 Score menu program
 """
+
+def main():
+    print(MENU)
+    choice = input(">>>: ").upper()
+    while choice != 'Q':
+        if choice == 'G':
+            pass
+        elif choice == 'P':
+            pass
+        elif choice == 'S':
+            pass
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>>: ").upper()
+    print("Farewell")
