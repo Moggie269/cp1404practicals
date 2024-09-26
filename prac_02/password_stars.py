d8c0965076a2952cbf465fbafbc3b79b371c7744
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-09-26 14:48:58 +1000

    Refactor password stars program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,25 @@
-"""CP1404 prac_02 - Password stars"""
-minimum_length = int(input('Enter minimum length: '))
-password = input("Insert password: ")
-while len(password) < minimum_length:
-    print("Invalid password")
-    password = input("Insert password: ")
-print("*" * len(password))
+"""
+CP1404/CP5632 - Practical
+Password stars
+"""
+
+
+def main():
+    minimum_length = int(input('Enter minimum length: '))
+    password = get_valid_password("Insert password: ", minimum_length)
+    print_stars(password)
+
+
+def get_valid_password(prompt, minimum_length):
+    password = input(prompt)
+    while len(password) < minimum_length:
+        print("Invalid password")
+        password = input(prompt)
+    return password
+
+
+def print_stars(password):
+    print("*" * len(password))
+
+
+main()
