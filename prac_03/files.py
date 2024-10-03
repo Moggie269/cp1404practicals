2e8586bd97664df6c9022667d030a1c4b5902cfa
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-10-03 15:50:02 +1000

    Add third task

--- prac_03/files.py
+++ prac_03/files.py
@@ -14,3 +14,10 @@ in_file = open('name.txt')
 new_name = in_file.read().strip()
 print(f"Hi {new_name}!")
 in_file.close()
+
+# 3.
+with open('numbers.txt') as in_file:
+    number_1 = int(in_file.readline())
+    number_2 = int(in_file.readline())
+    total = number_1 + number_2
+    print(total)
