484618d9fb2338e806ba87efdd37a0fec95654f4
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-10-03 14:45:29 +1000

    Add error checking loop for denominator

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,12 +9,13 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print('Denominator cannot be equal to 0!')
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")
 
 # 1. When will a ValueError occur? -> In case we input a string
