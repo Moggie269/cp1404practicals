0d9f8ae58cf4f7617d3321cdcb0c5cd1c0a1709f
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-10-10 15:06:29 +1000

    Add function to generate random values

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,7 +7,18 @@ import random
 def main():
     """"""
     number_of_quick_picks = int(input('How many quick picks do you want? '))
-    
+    values = []
+    generate_random_values(values)
+    print(sorted(values))
+
+
+def generate_random_values(values):
+    """Generate 6 random values."""
+    for i in range(6):
+        random_value = random.randint(1, 46)
+        while random_value in values:
+            random_value = random.randint(1, 46)
+        values.append(random_value)
 
 
 main()
