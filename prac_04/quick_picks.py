e44336ab40db66ceb798ef007be6519c63e91658
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-10-10 14:53:03 +1000

    Add initial code

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 Practical
+"Quick Pick" Lottery Ticket Generator
+"""
+import random
+
+def main():
+    """"""
+    number_of_quick_picks = int(input('How many quick picks do you want? '))
+    
+
+
+main()
