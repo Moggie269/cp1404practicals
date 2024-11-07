0f63369aaf1fe25389c9ed7e4853840e6f095860
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-11-07 14:59:15 +1000

    Add docstrings to functions

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -12,6 +12,7 @@ def main():
 
 
 def read_guitars(guitars):
+    """Read guitars from csv file."""
     with open('guitars.csv', 'r') as in_file:
         for line in in_file:
             parts = line.strip().split(',')
@@ -20,11 +21,13 @@ def read_guitars(guitars):
 
 
 def display_guitars(guitars):
+    """Display guitars."""
     for guitar in guitars:
         print(guitar)
 
 
 def add_guitars(guitars):
+    """Generate new guitars."""
     name = input('Name:')
     while name != '':
         year = int(input('Year:'))
@@ -36,6 +39,7 @@ def add_guitars(guitars):
 
 
 def save_guitars(guitars):
+    """Save guitars to csv file."""
     with open('guitars.csv', 'w') as out_file:
         for guitar in guitars:
             out_file.write(f'{guitar.name},{guitar.year},{guitar.cost}\n')
