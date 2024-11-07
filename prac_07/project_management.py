7bcb4f9194d2a3e80bf62308a4d178340c70204c
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-11-07 15:07:39 +1000

    Add menu

---
+++ prac_07/project_management.py
@@ -0,0 +1,37 @@
+"""
+CP1404/CP5632 Practical - Project Management.
+Estimate time: 30 minutes
+Actual time:
+"""
+import datetime
+MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
+        "- (U)pdate project\n- (Q)uit")
+
+
+def main():
+    """Display menu."""
+    print('Welcome to Pythonic Project Management')
+    print(MENU)
+    choice = input('>>> ').upper()
+    while choice != 'Q':
+        if choice == 'L':
+            pass
+        elif choice == 'S':
+            pass
+        elif choice == 'D':
+            pass
+        elif choice == 'F':
+            pass
+        elif choice == 'A':
+            pass
+        elif choice == 'U':
+            pass
+        else:
+            print('Invalid choice')
+        print(MENU)
+        choice = input('>>> ').upper()
+    print('Thank you for using custom-built project management software.')
+
+
+
+main()
