5453ed375394d3f0d6e093b5725f0c79d3c667c4
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-11-07 15:38:15 +1000

    Add code to save to a default file

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -34,7 +34,9 @@ def main():
             print('Invalid choice')
         print(MENU)
         choice = input('>>> ').upper()
-
+    save_choice = input('Would you like to save to projects.txt? ').upper()
+    if save_choice == 'Y':
+        save_default_projects(projects)
     print('Thank you for using custom-built project management software.')
 
 
@@ -46,6 +48,15 @@ def load_default_projects(projects):
             parts = line.strip().split('\t')
             project = Project(parts[0], parts[1], parts[2], parts[3], parts[4])
             projects.append(project)
+
+
+def save_default_projects(projects):
+    """Save projects to default file."""
+    with open('projects', 'w') as out_file:
+        out_file.write('Name	Start Date	Priority	Cost Estimate	Completion Percentage\n')
+        for project in projects:
+            out_file.write(project.__str__())
+            out_file.write('\n')
 
 
 main()
