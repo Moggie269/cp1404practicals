98058cc98d3b78b25aaf28d6bf03be1bca310bcc
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-11-07 15:28:03 +1000

    Add code to load from a default file

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,6 +4,7 @@ Estimate time: 30 minutes
 Actual time:
 """
 import datetime
+from project import Project
 MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
         "- (U)pdate project\n- (Q)uit")
 
@@ -11,6 +12,9 @@ MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter 
 def main():
     """Display menu."""
     print('Welcome to Pythonic Project Management')
+    projects = []
+    load_default_projects(projects)
+    print(f'Loaded {len(projects)} projects from projects.txt')
     print(MENU)
     choice = input('>>> ').upper()
     while choice != 'Q':
@@ -30,8 +34,18 @@ def main():
             print('Invalid choice')
         print(MENU)
         choice = input('>>> ').upper()
+
     print('Thank you for using custom-built project management software.')
 
+
+def load_default_projects(projects):
+    """Load projects from default file."""
+    with open('projects', 'r') as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split('\t')
+            project = Project(parts[0], parts[1], parts[2], parts[3], parts[4])
+            projects.append(project)
 
 
 main()
