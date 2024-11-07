6e34fa43516827f235bcb5f2a035f77127ff2515
Emiliano Mogorovich <emiliano.mogorovich@my.jcu.edu.au>
2024-11-07 15:12:13 +1000

    Add initial class methods

---
+++ prac_07/project.py
@@ -0,0 +1,16 @@
+class Project:
+    """Create Project instance."""
+
+    def __init__(self, name='', start_date='', priority=0, cost_estimate=0, completion_percentage=0):
+        """Initialize Project instance."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion_percentage}"
+
+    def __repr__(self):
+        return str(self)
